ff897a498928618d16ce858ef2341cfa71f08f52
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-07-02 15:31:42 +0200

    add mongoose, add controllers, change routes

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,58 +1,12 @@
 const express = require('express');
-const db = require('../db.js');
-const uuid = require('uuid').v4;
 const router = express.Router();
 
-router.route('/seats').get((req, res) => {
-    res.json(db.seats);
-});
+const SeatController = require('../controllers/seats.controller');
 
-router.route('/seats/:id').get((req, res) => {
-    res.json(db.seats.find((seats) => seats.id === +req.params.id));
-});
-
-router.route('/seats').post((req, res) => {
-    const { day, seat, client, email } = req.body;
-    const id = uuid();
-    const newSeats = { id: id, day, seat, client, email };
-    if (db.seats.some((seatCheck) => seatCheck.day == newSeats.day && seatCheck.seat == newSeats.seat)) {
-        res.json({ message: 'The slot is already taken' });
-        res.status(409).json({ message: 'The slot is already taken!' });
-    } else {
-        db.seats.push(newSeats);
-        req.io.emit('seatsUpdated', db.seats);
-        res.json({ message: 'ok!' });
-    }
-});
-
-router.route('/seats/:id').delete(
-    (req, res) => {
-        const id = +req.params.id;
-        db.seats.splice(
-            db.seats.findIndex((seat) => seat.id === id),
-            1
-        );
-        res.json({ message: 'Seat deleted' });
-    },
-    (err) => {
-        console.log(err);
-    }
-);
-
-router.route('/seats/:id').put(
-    (req, res) => {
-        const { day, seat, client, email } = req.body;
-        const id = +req.params.id;
-        const newSeats = db.seats.find((seat) => seat.id === id);
-        newSeats.day = day;
-        newSeats.seat = seat;
-        newSeats.client = client;
-        newSeats.email = email;
-        res.json({ message: 'ok!' });
-    },
-    (err) => {
-        console.log(err);
-    }
-);
+router.get('/seats', SeatController.getAll);
+router.get('/seats/:id', SeatController.getById);
+router.post('/seats', SeatController.addSeat);
+router.delete('/seats/:id', SeatController.delSeat);
+router.put('/seats/:id', SeatController.updateSeat);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const uuid = require('uuid');
 const router = express.Router();
 const app = express();
@@ -35,6 +36,14 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
